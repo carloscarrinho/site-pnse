f0f3b42a7939d8c0ffdc30e3a2f63db5ff361038
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-04-16 10:51:44 -0300

    Criação de funções auxiliares para URLs, datas e senhas

--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -150,3 +150,151 @@ function str_limit_chars(string $string, int $limit, string $pointer = "..."): s
     return "{$chars} {$pointer}";
 }
 
+
+/**
+ * ##############
+ * ###  URLs  ###
+ * ##############
+ */
+
+
+/**
+ * Função 'Helper' que cria uma URL validando se está em ambiente de teste ou de produção.
+ * pode receber string com o 'path' específico e retorna a URL identificando se $_SERVER é 'localhost' ou produção.
+ *
+ * @param  string $path
+ * @return string
+ */
+function url(string $path = null): string
+{
+    // TODO: take a look on this function
+    if($_SERVER['HTTP_HOST'] === "localhost"){
+        if($path){
+            return CONF_URL_TEST . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        }
+        return CONF_URL_TEST;
+    }
+
+    if($path){
+        return CONF_URL_BASE . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    }
+    return CONF_URL_BASE;
+}
+
+/**
+ * Função 'Helper' que monta uma url a partir do tema parametrizado no Config.php
+ *
+ * @param  string $path
+ * @return string
+ */
+function theme(string $path = null): string
+{
+    if($_SERVER['HTTP_HOST'] === "localhost"){
+        if($path){
+            return CONF_URL_TEST . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+        }
+        return CONF_URL_TEST;
+    }
+
+    if($path){
+        return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME . "/" . ($path[0] == "/" ? mb_substr($path, 1) : $path);
+    }
+    return CONF_URL_BASE . "/themes/" . CONF_VIEW_THEME;
+}
+
+/**
+ * Função 'Helper' que redireciona para uma determinada URL informada.
+ * recebe uma string, testa se ela é válida e redireciona para URL informada.
+ *
+ * @param  string $url
+ * @return void
+ */
+function redirect(string $url): void
+{
+    header('HTTP/1.1 302 Redirect');
+    if(filter_var($url, FILTER_VALIDATE_URL)){
+        header("Location: {$url}");
+        exit;
+    }
+
+    $location = url($url);
+    header("Location: {$location}");
+    exit;
+}
+
+
+/**
+ * ###############
+ * ###  DATES  ###
+ * ###############
+ */
+
+
+/**
+ * Função 'Helper' que cria um atalho para formatação de datas.
+ * pode ou não receber duas strings: 'date' (data a formatar) e 'format' (formato desejado)
+ * caso não receba, por padrão ela retornará uma string com a data e hora atual no formato d/m/Y H\hi. 
+ *
+ * @param  string $date
+ * @param  string $format
+ * @return string
+ */
+function date_fmt(string $date = "now", string $format = "d/m/Y H\hi"): string
+{
+    // return (date_format((new DateTime($date)), $format));
+    return (new DateTime($date))->format($format);
+}
+
+/**
+ * Função 'Helper' que cria atalho para formatação da data no formato brasileiro, parametrizado no Config.php
+ *
+ * @param  string $date
+ * @return string
+ */
+function date_fmt_br(string $date = 'now'): string
+{
+    return (new DateTime($date))->format(CONF_DATE_BR);
+}
+
+/**
+ * Função 'Helper' que cria atalho para formatação da data do APP, parametrizado no Config.php
+ *
+ * @param  string $date
+ * @return string
+ */
+function date_fmt_app(string $date = 'now'): string
+{
+    return (new DateTime($date))->format(CONF_DATE_APP);
+}
+
+
+/**
+ * ##################
+ * ###  PASSWORD  ###
+ * ##################
+ */
+
+
+/**
+ * Função 'Helper' que cria um atalho para criptografia da senha conforme parametrizado no Config.php
+ *
+ * @param  string $password
+ * @return string
+ */
+function passwd(string $password): string
+{
+    return password_hash($password, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
+}
+
+/**
+ * Função 'Helper' que cria um atalho para rehash da senha.
+ *
+ * @param  string $hash
+ * @return bool
+ */
+function passwd_rehash(string $hash): bool
+{
+    return password_needs_rehash($hash, CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
+}
+
+// TODO: create CSRF helper functions.
