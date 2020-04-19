84635368ac8eec1b24a93c0d2f7f25e878eff1ba
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-04-19 20:31:33 -0300

    Ajuste na estrutura de pastas do projeto

---
+++ src/Boot/Minify.php
@@ -0,0 +1,31 @@
+<?php
+
+use MatthiasMullie\Minify\CSS;
+
+if(strpos($_SERVER['HTTP_HOST'], "localhost")){
+    /**
+     * CSS
+     */
+    $minCSS = new CSS();
+    $minCSS->add(__DIR__ . "/../../shared/styles/boot.css");
+    $minCSS->add(__DIR__ . "/../../shared/styles/styles.css");
+    $minCSS->add(__DIR__ . "/../../shared/styles/formularios.css");
+
+    // theme
+    $cssDir = scandir(__DIR__ . "/../../themes/" . CONF_VIEW_THEME . "/assets/css");
+    foreach ($cssDir as $css) {
+        $cssFile = __DIR__ . "/../themes/" . CONF_VIEW_THEME . "/assets/css/{$css}";
+        if(is_file($cssFile) && pathinfo($cssFile)['extension'] == "css"){
+            $minCSS->add($cssFile);
+        };
+    }
+
+    // Minify CSS
+    $minCSS->minify(__DIR__ . "/../../themes/" . CONF_VIEW_THEME . "/assets/style.css");
+
+
+    /**
+     * MINIFICAR O JS
+     */
+
+}
