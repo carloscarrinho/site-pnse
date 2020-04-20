6720a33c19a1d442292031da3b4c93f41be2ac1f
Carlos Eduardo Ferreira Carrinho <carrinhoedu@gmail.com>
2020-04-19 21:11:34 -0300

    Implementação dos elementos da página home

--- themes/pilot/_theme.php
+++ themes/pilot/_theme.php
@@ -89,9 +89,88 @@
 
 <!-- CONTENT -->
 <main>
+    <!-- FORMULÁRIO MODAL -->
+    <div 
+    class="modal fade" 
+    id="formulario-modal" 
+    tabindex="-1" 
+    role="dialog" 
+    aria-labelledby="exampleModalLabel" 
+    aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <dialog class="modal-content">
+                <header class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Fale conosco!</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                    </button>
+                </header>
+                <main class="modal-body">
+                    <form>
+                    <div class="form-group">
+                        <label for="nome-usuario">Nome</label>
+                        <input type="text" class="form-control" id="nome-usuario" placeholder="Escreva seu nome aqui">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlInput1">Email</label>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="seuemail@exemplo.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Mensagem</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Escreva sua mensagem aqui" ></textarea>
+                    </div>
+                    </form>
+                </main>
+                <footer class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                    <button type="button" class="btn botao-padrao">Enviar</button>
+                </footer>
+            </dialog>
+        </div>
+    </div>
+
     <?= $v->section("content"); ?>
 </main>
 
+<!-- FOOTER -->
+<footer class="bg-success text-light m-0 p-2">
+    <div class="container-fluid">
+        <div class="row justify-content-between">
+            <section class="row justify-content-center m-0 p-3 col-12 col-md-4">
+                <div class="text-center">
+                    <h1>Inscreva-se!</h1>
+                    <p>Receba nossas novidades por e-mail</p>
+                </div>
+                <div class="text-center">
+                    <form>
+                        <div class="form-group">
+                            <label for="nome-usr-newsletter">Nome</label>
+                            <input type="text" class="form-control" id="nome-usr-newsletter" placeholder="Escreva seu nome aqui">
+                        </div>
+                        <div class="form-group">
+                            <label for="email-newsletter">Email</label>
+                            <input type="email" class="form-control" id="email-newsletter" placeholder="seuemail@exemplo.com">
+                        </div>
+                    </form>
+                </div>
+            </section>
+        
+            <section class="row justify-content-center m-0 col-12 col-md-8">
+                <div class="col-12">
+                    <div class="text-center"> 
+                        <img src="<?= theme("/assets/img/jesus-white.svg") ?>" class="text-light" alt="" width="200px">
+                    </div>
+                    <div class="text-center m-0 p-0">
+                        <p class="m-0">Secretaria Paroquial:</p>
+                        <p class="m-0">(27) XXXX-XXXX | XXXX-XXXX&nbsp;</p>
+                        <p class="m-0">Horário de atendimento da secretaria:&nbsp;</p>
+                        <p class="m-0">Segunda à Sexta: 08:00h às 12:00h | 13:30h às 19:00h</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
 
 <script 
 src="https://code.jquery.com/jquery-3.4.1.slim.min.js" 
@@ -107,5 +186,7 @@ crossorigin="anonymous">
 src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" 
 crossorigin="anonymous">
 </script>
+<script src="<?= theme("/assets/scripts.js"); ?>"></script>
+<?php $v->section("scripts"); ?>
 </body>
 </html>
--- themes/pilot/home.php
+++ themes/pilot/home.php
@@ -40,3 +40,159 @@
     </a>
 </div>
 
+<!-- FEATURE: SEÇÃO DE NOTÍCIAS -->
+<section class="container-fluid bg-light">
+    <!-- INÍCIO DA CHAMADA DE NOTÍCIAS -->
+        <div class="row">
+            <div class="p-4 col-12 col-md-12">
+                <h1 class="text-center">Principais notícias</h1>
+                <p class="text-center text-secondary">Fique por dentro dos principais acontecimentos de nossa paróquia e sociedade católica!</p>
+            </div>
+        </div>
+    <!-- FIM DA CHAMADA DE NOTÍCIAS -->
+
+    <!-- CAROUSEL DE NOTICIAS -->
+        <div class="row justify-content-center">
+            <div id="carrossel-noticia" 
+            class="carousel slide col-12 col-md-10 p-2" 
+            data-interval="10000" 
+            data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carrossel-noticia" data-slide-to="0" class="active"></li>
+                    <li data-target="#carrossel-noticia" data-slide-to="1"></li>
+                    <li data-target="#carrossel-noticia" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img 
+                        src="<?= theme("/assets/img/cf-banner-2020-02.jpg"); ?>" 
+                        class="d-block w-100 imagem-carrossel-noticias" 
+                        alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <!-- <h2 class="display-3">Campanha da Fraternidade 2020</h2>
+                            <p>"Viu, sentiu compaixão e cuidou dele."(Lc 10:33-35)</p> -->
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img 
+                        src="<?= theme("/assets/img/comunicado-coronavirus-2020.png"); ?>" 
+                        class="d-block w-100 imagem-carrossel-noticias" 
+                        alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <!-- <h2 class="display-3">Second slide label</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> -->
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img 
+                        src="<?= theme("/assets/img/quaresma-2020-01.jpg"); ?>" 
+                        class="d-block w-100 imagem-carrossel-noticias" 
+                        alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h2 class="display-3">Quaresma</h2>
+                            <p>Tempo de Conversão</p>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carrossel-noticia" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carrossel-noticia" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+    <!-- FIM DO CAROUSEL DE NOTÍCIAS -->
+</section>
+
+<!-- FEATURE: SEÇÃO DE ACESSO À INFORMAÇÃO -->
+<section class="container-fluid p-5">
+    <!-- ÍCONES DE SERVIÇOS -->
+    <div class="row">
+            <div class="p-4 col-12 col-md-12">
+                <h1 class="text-center">Acesso à informação</h1>
+                <!-- <p class="text-center text-secondary">Encontre o que você deseja!</p> -->
+            </div>
+        </div>
+
+    <div class="row">
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <div class="">
+                <a class="" href="#">
+                    <div class="text-center">
+                        <img src="<?= theme("/assets/img/calendar.svg"); ?>"  width="74">
+                    </div>
+                    <div class="">
+                        <p class="text-center p-2">Agenda</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <a class="" href="http://www.catolicoorante.com.br/liturgia_diaria.php" target="_blank">
+                <div class="text-center">
+                    <img src="<?= theme("/assets/img/lectern.svg"); ?>" alt="icon-liturgia" width="74">
+                </div>
+                <div class="desc_wrapper">
+                    <p class="text-center p-2">Liturgia</p>
+                </div>
+            </a>
+        </div>
+        
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <div>
+                <a class="" href="#">
+                    <div class="text-center">
+                        <img src="<?= theme("/assets/img/eucharist.svg"); ?>"alt="icone-missa" width="74">
+                    </div>
+                    <div class="">
+                        <p class="text-center p-2">Missas</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <div>
+                <a class="" href="#">
+                    <div class="text-center">
+                        <img src="<?= theme("/assets/img/church.svg"); ?>" width="74">
+                    </div>
+                    <div class="">
+                        <p class="text-center p-2">Comunidades</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <!-- class="icon_box icon_position_top no_border" -->
+            <div>
+                <a class="" href="#">
+                    <div class="text-center">
+                        <img src="<?= theme("/assets/img/jesus.svg"); ?>" width="74">
+                    </div>
+                    <div class="">
+                        <p class="text-center p-2">Reflexões</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        <div class="p-4 col-12 col-md-4 col-lg-2">
+            <div>
+                <a class="" href="#">
+                    <div class="text-center">
+                        <img src="<?= theme("/assets/img/balloon.svg"); ?>" width="74">
+                    </div>
+                    <div class="">
+                        <p class="text-center p-2">Festas</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
+
